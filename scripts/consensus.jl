954cfd731549a49de3f8f3d550f9f5e0c0f33b2c
appankow <appankow@uw.edu>
2020-05-13 11:48:15 -0700

    switched consensus to consensus_seq()

--- scripts/consensus.jl
+++ scripts/consensus.jl
@@ -1,3 +1,80 @@
+using Distributed
+@everywhere ENV["MPLBACKEND"] = "Agg"
+@everywhere using RobustAmpliconDenoising, NextGenSeqUtils
+
+function generateConsensusFromDir(dir, template_name)
+    files = [dir*"/"*f for f in readdir(dir) if f[end-5:end] == ".fastq"]
+    if length(files) > 0
+        println("Generating consensus for $(length(files)) templates")
+    else
+        println("WARNING: no template families for $(template_name)")
+        exit()
+    end
+    cons_collection = pmap(ConsensusFromFastq, files)
+    seq_collection = [i[1] for i in cons_collection]
+    seqname_collection = [template_name*i[2] for i in cons_collection]
+    return seq_collection, seqname_collection
+end
+
+@everywhere function ConsensusFromFastq(file)
+    seqs,phreds,seq_names = read_fastq(file)
+    draft = consensus_seq(seqs)
+    draft2 = refine_ref(draft, seqs)
+    final_cons = refine_ref(draft2,seqs)
+    alignments, maps, matches, matchContent = getReadMatches(final_cons, seqs, 0)
+    cons_name = split(basename(file),".")[1]*" num_CCS=$(length(seqs)) min_agreement=$(round(minimum(matches); digits = 2))"
+    return final_cons, cons_name
+end
+
+@everywhere begin
+"""
+Returns an array of degapped coordinates, such that
+coords(ref, read)[i] gives you the position the aligned read/ref
+that matches the i'th ungapped position in ref.
+"""
+function coords(ref, read)
+    if length(ref) != length(read)
+        error("Aligned strings are meant to be the same length.")
+    end
+    degappedRef = degap(ref)
+    coordMap = zeros(Int64, length(degappedRef))
+    count = 1
+    for i in 1:length(degappedRef)
+        while ref[count] == '-'
+            count += 1
+        end
+        coordMap[i] = count
+        count += 1
+    end
+    return coordMap
+end
+end
+
+@everywhere begin
+"""
+Return matches to a candidate reference from a set of reads.
+"""
+function getReadMatches(candidate_ref, reads, shift; degap_param = true, kmer_align = true)
+    alignments = []
+    if kmer_align
+        alignments = map(i -> kmer_seeded_align(candidate_ref, i), reads)
+    else
+        alignments = map(i -> nw_align(candidate_ref, i), reads)
+    end
+
+    maps = [coords(i...) for i in alignments]
+
+    if (degap_param)
+        matchContent = [[degap(alignments[i][2][maps[i][k]:maps[i][k+shift]]) for i in 1:length(maps)] for k in 1:length(candidate_ref)-shift]
+        matches = [freq(matchContent[k], degap(candidate_ref[k:k+shift])) for k in 1:length(matchContent)]
+    else
+       matchContent = [[(alignments[i][2][maps[i][k]:maps[i][k+shift]]) for i in 1:length(maps)] for k in 1:length(candidate_ref)-shift]
+       matches = [freq(matchContent[k], candidate_ref[k:k+shift]) for k in 1:length(matchContent)]
+    end
+    return alignments, maps, matches, matchContent
+end
+end
+
 #load required packages
 using Distributed
 @everywhere ENV["MPLBACKEND"] = "Agg"
@@ -37,7 +114,7 @@ end
 
 #Calculate consensus sequences for each family.
 t1 = time()
-cons_collection, name_collection = denoiseDir(ARGS[1])
+cons_collection, name_collection = generateConsensusFromDir(ARGS[1], "")
 write_fasta(ARGS[2],
     cons_collection;
     names = name_collection)
